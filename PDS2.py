e51c416631c96ea5e6fc22a1de11dacb7a5e91f5
Casey Charlesworth <cacklehousefilms@gmail.com>
2017-05-03 19:41:09 +1000

    Uploading clean python shell

---
+++ PDS2.py
@@ -0,0 +1,12 @@
+#s3132392 Casey-Ann Charlesworth
+#Assignment 2 - Practical Data Science
+#Assignment due 18 May 2017
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+print("We are totally awesome ;)")
