4c461eb0b87744fe3a3ef7bd581200ded9e55886
Casey Charlesworth <cacklehousefilms@gmail.com>
2017-05-08 20:43:07 +1000

    Models complete and report started

--- PDS2_model.py
+++ PDS2_model.py
@@ -9,40 +9,16 @@ import numpy as np
 import itertools
 from sklearn.cross_validation import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 
-files = ["red", "white"]
-var = ["fixed acidity","volatile acidity","citric acid","residual sugar",
-       "chlorides","free sulfur dioxide","total sulfur dioxide","density",
-       "pH","sulphates","alcohol"]
-class_var = ["quality"]
-target_names = ['3','4','5','6','7','8']
-k = 100
-
-
-#import file and create summary graphs
-for f in files:
-    filename = f + ".csv"
-    wineq = pd.read_csv(filename,sep=",",header=0)
-
-    #K nearest neighbours
-    y = wineq.quality.values
-    X = wineq[var].values
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.4,random_state=0)
-    clf = KNeighborsClassifier(k)
-    fit = clf.fit(X_train,y_train)
-    predicted = fit.predict(X_test)
-    cm = confusion_matrix(y_test, predicted)
-    cr = classification_report(y_test, predicted)
-
-    #Calculate confusion error rate
-    cer = accuracy_score(y_test, predicted)
-    
-    #Print confusion matrix
-    plt.imshow(cm, cmap=plt.cm.Purples)
-    plt.title("KNN confusion matrix: " + f + " wine (k=" + str(k) + ")\n Classification error rate = " + str(round(cer,3)))
+#Function to print confusion matrix
+def print_confusion_matrix(cm, title, cer, color):
+    plt.imshow(cm, cmap=color)
+    plt.title(title)
     
     plt.ylabel("True label")
     plt.xlabel("Predicted label")
@@ -58,6 +34,92 @@ for f in files:
 
     plt.tight_layout()
     plt.show()
+
+#Define variables
+files = ["red", "white"]
+var = ["fixed acidity","volatile acidity","citric acid","residual sugar",
+       "chlorides","free sulfur dioxide","total sulfur dioxide","density",
+       "pH","sulphates","alcohol"]
+class_var = ["quality"]
+
+
+#import file and create summary graphs
+for f in files:
+    filename = f + ".csv"
+    wineq = pd.read_csv(filename,sep=",",header=0)
+
+    if f == "red":
+        target_names = ['3','4','5','6','7','8']
+    elif f == "white":
+        target_names = ['3','4','5','6','7','8','9']
+
+    #K nearest neighbours
+    k = 1
+    y = wineq.quality.values
+    X = wineq[var].values
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=4)
+    clf = KNeighborsClassifier(k)
+    fit = clf.fit(X_train,y_train)
+    predicted = fit.predict(X_test)
+    cm = confusion_matrix(y_test, predicted)
+    cr = classification_report(y_test, predicted)
+
+    #Calculate confusion error rate
+    acc = accuracy_score(y_test, predicted)
+    cer = 1-acc
+    
+    #Print confusion matrix
+    print_confusion_matrix(cm, title="KNN confusion matrix: " + f + " wine (k=" + str(k) + ")\n Classification error rate = "
+                           + str(round(cer,3)), cer=cer, color=plt.cm.Purples)
+
+    #Print classification report (to screen)
+    print(cr)
+
+    #Decision tree
+    max_d=5 #This did not assist
+    min_ss=10 #No
+    min_sl=10 #No
+    max_f=3 #Made a big difference - keep in
+    max_ln=3 #No
+    y = wineq.quality.values
+    X = wineq[var].values
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=4)
+    clf = DecisionTreeClassifier(max_features=max_f)
+    fit = clf.fit(X_train,y_train)
+    predicted = fit.predict(X_test)
+    cm = confusion_matrix(y_test, predicted)
+    cr = classification_report(y_test, predicted)
+
+    #Calculate confusion error rate
+    acc = accuracy_score(y_test, predicted)
+    cer = 1-acc
+
+    #Print confusion matrix
+    print_confusion_matrix(cm, title="Decision tree confusion matrix: " + f + " wine \n" +
+                           "(max features=" + str(max_f) + ")\n Classification error rate = "
+                           + str(round(cer,3)), cer=cer, color=plt.cm.Oranges)
+
+    #Print classification report (to screen)
+    print(cr)
+
+
+    #Naive Bayes
+    y = wineq.quality.values
+    X = wineq[var].values
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=4)
+    clf = GaussianNB()
+    fit = clf.fit(X_train,y_train)
+    predicted = fit.predict(X_test)
+    cm = confusion_matrix(y_test, predicted)
+    cr = classification_report(y_test, predicted)
+    
+    #Calculate confusion error rate
+    acc = accuracy_score(y_test, predicted)
+    cer = 1-acc
+    
+    #Print confusion matrix
+    print_confusion_matrix(cm, title="Naive Bayes confusion matrix: " + f + " wine \n Classification error rate = "
+                           + str(round(cer,3)), cer=cer, color=plt.cm.Blues)
 
     #Print classification report (to screen)
     print(cr)
